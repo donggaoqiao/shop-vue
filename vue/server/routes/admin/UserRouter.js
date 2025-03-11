a9b17119c5ea59e1c75714e2a8ea464adad1370d
doom <2141814353@qq.com>
2025-03-11 19:44:58 +0800

    第一次完成后台系统

--- vue/server/routes/admin/UserRouter.js
+++ vue/server/routes/admin/UserRouter.js
@@ -4,8 +4,5 @@ var express = require('express');
 var UserRouter = express.Router();
 
 UserRouter.post("/adminapi/user/login",UserController.login)
-UserRouter.get("/adminapi/user/home",(req,res)=>{
-    res.send({ok:1})
-})
 
 module.exports = UserRouter;
